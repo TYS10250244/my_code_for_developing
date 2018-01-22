15fad42a94282258237985964a9eda1bdab29def
TYS10250244 <34406082+TYS10250244@users.noreply.github.com>
2018-01-23 06:53:33 +0900

    Add files via upload

---
+++ Calc_cov.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 06:44:19 2018
+
+@author: katou
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import table
+import seaborn as sns 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import table
+import seaborn as sns 
+from sklearn.decomposition import PCA
+import warnings;warnings.filterwarnings('ignore')
+from pandas import Series, DataFrame
+from scipy.optimize import minimize
+from scipy.cluster.hierarchy import ward, dendrogram
+from sklearn import  covariance
+
+
+def cov_mat(df_ret,lookbak):
+    return DataFrame.cov(ret0[-1*lookbak:])
+    
+
+def cov_mat_L1p(df_ret,lookbak):
+    G_cov = G_LassoCV.fit(ret0[-1*lookbak:].dropna(axis=0))
+    return  pd.DataFrame(G_cov.covariance_)* float((lookbak)/(lookbak-1))
